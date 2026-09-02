36862815eadb3a623dd8e0bdb9e90a85f8843aa4
Rockynaveen <naveengandham970@gmail.com>
2026-09-02 15:11:23 +0530

    Shift header logo further left

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -51,9 +51,9 @@ export const Navbar: React.FC = () => {
           : 'bg-white/90 backdrop-blur-sm py-1 sm:py-1.5'
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+      <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
         {/* Brand Logo with exact official iconography */}
-        <Link to="/" className="flex items-center group -ml-1 sm:-ml-1.5">
+        <Link to="/" className="flex items-center group -ml-2 sm:-ml-4 lg:-ml-6">
           <Logo size="md" theme="light" />
         </Link>
 
